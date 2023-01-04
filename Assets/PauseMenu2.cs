a2ed2e3a523657696da3f5fa50c19bcf171f2278
WaadHammami <118201409+WaadHammami@users.noreply.github.com>
2023-01-04 22:16:37 +0100

    add

---
+++ Assets/PauseMenu2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu2 : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+   
+    // Update is called once per frame
+
+
+    public void LoadMenuPrincipal()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenuExp", LoadSceneMode.Single);
+
+
+    }
+
+    public void LoadMenuPhobieSoc()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SocialPhobiaMenu", LoadSceneMode.Single);
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    public void Pause()
+    {
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.Start) == true)
+        {
+
+            Pause();      
+
+        }
+    }
+
+    public void LoadMenu()
+    {
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenuExp", LoadSceneMode.Single);
+        Debug.Log("Loading Menu ...");
+    }
+
+    public void QuitGame()
+    {
+
+        Debug.Log("Quitting game ...");
+        Application.Quit();
+    }
+}
